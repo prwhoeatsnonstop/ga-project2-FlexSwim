117fe9352156a64f9b4421ab33b17de1c915ebcb
eunicelok <eunicelp511@gmail.com>
2020-02-04 10:00:24 +0800

    Change some layout for home page and show all workout page

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -44,63 +44,7 @@ class Layout extends React.Component {
       </div>
     </nav>
             {this.props.children}
-  <div className="container" align="center">
-<div className="row text-center">
 
-      <div className="col-lg-3 col-md-6 mb-4">
-        <div className="card h-100">
-          <img className="card-img-top" src="http://placehold.it/500x325" alt="image"/>
-          <div className="card-body">
-            <h4 className="card-title">Card Title</h4>
-            <p className="card-text">This is where you see your swimming workouts.</p>
-          </div>
-          <div className="card-footer">
-            <a href="/" className="btn btn-primary">Find Out More!</a>
-          </div>
-        </div>
-      </div>
-
-      <div className="col-lg-3 col-md-6 mb-4">
-        <div className="card h-100">
-          <img className="card-img-top" src="http://placehold.it/500x325" alt=""/>
-          <div className="card-body">
-            <h4 className="card-title">Card title</h4>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
-          </div>
-          <div className="card-footer">
-            <a href="#" className="btn btn-primary">Find Out More!</a>
-          </div>
-        </div>
-      </div>
-
-      <div className="col-lg-3 col-md-6 mb-4">
-        <div className="card h-100">
-          <img className="card-img-top" src="http://placehold.it/500x325" alt=""/>
-          <div className="card-body">
-            <h4 className="card-title">Card title</h4>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-          </div>
-          <div className="card-footer">
-            <a href="#" className="btn btn-primary">Find Out More!</a>
-          </div>
-        </div>
-      </div>
-
-      <div className="col-lg-3 col-md-6 mb-4">
-        <div className="card h-100">
-          <img className="card-img-top" src="http://placehold.it/500x325" alt=""/>
-          <div className="card-body">
-            <h4 className="card-title">Card title</h4>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
-          </div>
-          <div className="card-footer">
-            <a href="#" className="btn btn-primary">Find Out More!</a>
-          </div>
-        </div>
-      </div>
-
-    </div>
-</div>
 
         </body>
         <footer className="page-footer font-small blue">
